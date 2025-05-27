70df4c49bb16ef4d87ddd52d929205678d4b543c
layo <layo_a@live.com>
2025-05-27 10:23:23 +0100

    feat: developing the section for the gatys model

--- app.py
+++ app.py
@@ -239,6 +239,12 @@ with tab3:
         
 with tab4:
       st.markdown('<h3 style="text-align:center;">Gatys model</h3>', unsafe_allow_html=True)
+      st.markdown('<h4 style="text-align:center;">Provides the highest style quality at the cost of speed (will take around 5 minutes)</h4>', unsafe_allow_html=True)
+      epoch_slider = st.slider(
+          "Select Epochs",
+          min_value=1, max_value=1000, value=10, step=1,
+          help="Set the number of epochs for the style transfer. More epochs may yield better results but will take longer."
+      )
     
     
 
