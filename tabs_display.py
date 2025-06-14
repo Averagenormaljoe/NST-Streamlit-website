d03ff598832f3b60e21b8ef84b535b9e666d57d2
layo <layo_a@live.com>
2025-06-14 18:42:45 +0100

    feat: hidden the other display tabs in the application for production purposes

--- tabs_display.py
+++ tabs_display.py
@@ -7,21 +7,21 @@ from tabs.video_tab import video_tab
 from tabs.huang_tab import huang_tab
 from tabs.default_tab import default_tab
 def tabs_display():
-    tab_list : list[str] = ["Image", "Video", "Johnson model", "Gatys model", "Huang model"]
-    tab1, tab2, tab3,tab4,tab5 = st.tabs(tab_list)
+    tab_list : list[str] = ["Image"]
+    tab1 = st.tabs(tab_list)
 
     # -------------Header Section------------------------------------------------
 
     with tab1:
         header()
-    with tab2:
-        header()
-    with tab3:
-        header()
-    with tab4:
-        header()
-    with tab5:
-        header()
+    # with tab2:
+    #     header()
+    # with tab3:
+    #     header()
+    # with tab4:
+    #     header()
+    # with tab5:
+    #     header()
 
 
     # -------------Sidebar Section------------------------------------------------
@@ -39,18 +39,18 @@ def tabs_display():
     with tab1:
         default_tab()
 
-    # -------------Video Style Transfer Section------------------------------------------------
+    # # -------------Video Style Transfer Section------------------------------------------------
 
-    with tab2:
-        video_tab()
+    # with tab2:
+    #     video_tab()
                 
-    # -------------Johnson Model Section------------------------------------------------             
-    with tab3:
-        johnson_tab()
-    # -------------Gatys Model Section------------------------------------------------        
-    with tab4:
-        gatys_tab()
-    # -------------Huang Model Section------------------------------------------------    
+    # # -------------Johnson Model Section------------------------------------------------             
+    # with tab3:
+    #     johnson_tab()
+    # # -------------Gatys Model Section------------------------------------------------        
+    # with tab4:
+    #     gatys_tab()
+    # # -------------Huang Model Section------------------------------------------------    
 
-    with tab5:
-        huang_tab()
+    # with tab5:
+    #     huang_tab()
