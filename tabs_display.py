f1aaa0cf7c646dd453ac07246704a0ad8567b6ef
layo <layo_a@live.com>
2025-06-24 06:11:25 +0100

    feat: removed tabs from the project due to video being incomplete

--- tabs_display.py
+++ tabs_display.py
@@ -7,13 +7,11 @@ from tabs.video_tab import video_tab
 from tabs.huang_tab import huang_tab
 from tabs.default_tab import default_tab
 def tabs_display():
-    tab_list : list[str] = ["Image", "Video", "Johnson model", "Gatys model", "Huang model"]
-    tab1 = st.tabs(tab_list[0])
-
+  
     # -------------Header Section------------------------------------------------
 
-    with tab1:
-        header()
+    
+    header()
   
     # with tab3:
     #     header()
@@ -35,8 +33,8 @@ def tabs_display():
     # ----------------------------------------------------------------------
 
     # -------------Body Section------------------------------------------------
-    with tab1:
-        default_tab()
+    
+    default_tab()
 
     # # -------------Video Style Transfer Section------------------------------------------------
                 
