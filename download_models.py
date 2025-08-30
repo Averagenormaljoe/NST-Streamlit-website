979db46f9bf2a923d21cec964e7280d7c75c1fa4
layo <layo_a@live.com>
2025-08-30 18:56:07 +0100

    feat: added script to download streamlit models prior to execution, which will call using the 'setup.sh' file in the root, also added gdown package to the project

---
+++ download_models.py
@@ -0,0 +1,22 @@
+import gdown
+import zipfile
+
+
+def download_model(id, output_name, extracted_name):
+    url = f"https://drive.google.com/uc?export=download&id={id}"
+    gdown.download(url, output_name, quiet=False)
+    
+    with zipfile.ZipFile(output_name, 'r') as f:
+        f.extractall(extracted_name)
+
+
+file_url_1 = "1MOm62RcSUFxrd1ROykidFzsGGZds1ENf"
+output_name_1 = "forward_model.zip"
+extracted_name_1 = "forward_feed"
+
+file_url_2 = "18eDaP-1QOR2Y6Jzog5wFQp6Q5XwTdSUx"
+output_name_2 = "main_model.zip"
+extracted_name_2 = "main_model"
+
+download_model(file_url_1, output_name_1, extracted_name_1)
+download_model(file_url_2, output_name_2, extracted_name_2)
