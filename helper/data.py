a4bd93ec867030502c899a13038d489a6dd3549e
layo <layo_a@live.com>
2025-08-30 16:12:07 +0100

    feat: renamed 'a' variable to 'sub'

--- helper/data.py
+++ helper/data.py
@@ -5,6 +5,6 @@ if not os.path.exists(forward_feed_model_dir):
     raise ValueError(f"Error: The ({forward_feed_model_dir}) directory does not exist.")
 
 
-subdirs = [a for a in os.listdir(forward_feed_model_dir) if os.path.isdir(os.path.join(forward_feed_model_dir, a))]
+subdirs = [sub for sub in os.listdir(forward_feed_model_dir) if os.path.isdir(os.path.join(forward_feed_model_dir, sub))]
 style_models_name : list[str] = subdirs
 style_models_dict : dict[str,str]  = {name: os.path.join(forward_feed_model_dir, name) for name in subdirs}
