1cd4f0fa97ea3234f0b61b9ae59256185d8d3678
layo <layo_a@live.com>
2025-08-31 08:37:03 +0100

    feat: made the stylemotion model, the only selectable model for A/B testing of the project

--- download_models.py
+++ download_models.py
@@ -13,13 +13,9 @@ def download_model(id, output_name, extracted_name):
         f.extractall(extracted_name)
 
 
-file_url_1 = "1MOm62RcSUFxrd1ROykidFzsGGZds1ENf"
-output_name_1 = "forward_model.zip"
-extracted_name_1 = "forward_model"
 
-file_url_2 = "1r8E-kicBnwE85MwFnQkKUlHQxFAn22Ik"
-output_name_2 = "main_model.zip"
-extracted_name_2 = "main_model"
+file_url_1 = "1r8E-kicBnwE85MwFnQkKUlHQxFAn22Ik"
+output_name_1 = "main_model.zip"
+extracted_name_1 = "main_model"
 
 download_model(file_url_1, output_name_1, extracted_name_1)
-download_model(file_url_2, output_name_2, extracted_name_2)
--- helper/tabs_display.py
+++ helper/tabs_display.py
@@ -1,20 +1,15 @@
 import streamlit as st
 
 from helper.UI_components import example_images, header
-from tabs.johnson_tab import johnson_tab
 from tabs.default_tab import default_tab
 def tabs_display():
-    tab_list : list[str] = ["Main (StyleMotion)", "Johnson model", "Gatys model"]
-    tab1, tab2 = st.tabs(tab_list[0:2])
+    tab_list : list[str] = ["Main (StyleMotion)"]
+    tab1 = st.tabs(tab_list)
 
     # -------------Header Section------------------------------------------------
 
     with tab1:
         header()
-
-    with tab2:
-        header() 
-
 
 
     # -------------Sidebar Section------------------------------------------------
@@ -31,10 +26,3 @@ def tabs_display():
     # -------------Body Section------------------------------------------------
     with tab1:
         default_tab()
-
-                
-    # # -------------Johnson Model Section------------------------------------------------             
-    with tab2:
-         johnson_tab()
-    # # -------------Gatys Model Section------------------------------------------------        
-
