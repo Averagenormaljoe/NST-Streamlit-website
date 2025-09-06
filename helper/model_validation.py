34ed4fc219ba3224a56e5da2463147f69ce50942
layo <layo_a@live.com>
2025-09-06 14:51:39 +0100

    feat: updated 'is_forward_feed' to use the correct directory name

--- helper/model_validation.py
+++ helper/model_validation.py
@@ -1,7 +1,7 @@
 import os
 def is_AdaIN(model_path : str) -> bool:
     return "AdaIN" in model_path or model_path.startswith("main_model")
-def is_forward_feed(model_path : str) -> bool:
-    return "forward_feed" in model_path and variables_dir_exists(model_path)
+def is_forward_feed(model_path : str, model_name) -> bool:
+    return "forward_model" in model_path and variables_dir_exists(model_path)
 def variables_dir_exists(style_model_path: str) -> bool:
     return os.path.exists(os.path.join(style_model_path, "variables"))
