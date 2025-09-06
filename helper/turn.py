72994ed222c2c684808057d6a6ad82e4a8810550
layo <layo_a@live.com>
2025-09-06 14:13:43 +0100

    feat: changed code to only use stun server from google

--- helper/turn.py
+++ helper/turn.py
@@ -9,20 +9,4 @@ logger = logging.getLogger(__name__)
 
 @st.cache_data 
 def get_ice_servers():
-
-    try:
-        account_sid : str = os.environ["TWILIO_ACCOUNT_SID"]
-        auth_token : str = os.environ["TWILIO_AUTH_TOKEN"]
-    except KeyError:
-        logger.warning(
-            "Twilio credentials are not set. Fallback to a free STUN server from Google."  # noqa: E501
-        )
-        return [{"urls": ["stun:stun.l.google.com:19302"]}]
-
-    try:
-        client = Client(account_sid, auth_token)
-
-        token = client.tokens.create()
-    except Exception as e:
-        print(f"Error while getting credentials: {e}") 
-    return token.ice_servers
+    return [{"urls": ["stun:stun.l.google.com:19302"]}]
