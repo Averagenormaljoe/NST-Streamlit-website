ed8f0b0f6c86263e281db44580627659db65586f
layo <layo_a@live.com>
2025-09-06 14:33:39 +0100

    feat: collected changes from the johnson tab

--- helper/tabs_display.py
+++ helper/tabs_display.py
@@ -2,16 +2,18 @@ import streamlit as st
 
 from helper.UI_components import example_images, header
 from tabs.johnson_tab import johnson_tab
+from tabs.default_tab import default_tab
 def tabs_display():
-    tab_list : list[str] = ["Johnson model"]
-    tab1, = st.tabs(tab_list)
+    tab_list : list[str] = ["Main (StyleMotion)", "Johnson model", "Gatys model"]
+    tab1, tab2 = st.tabs(tab_list[0:2])
 
     # -------------Header Section------------------------------------------------
 
     with tab1:
         header()
-        johnson_tab()
 
+    with tab2:
+        header() 
 
 
 
@@ -23,3 +25,16 @@ def tabs_display():
         # ---------------------Example art images------------------------------
 
         example_images()
+
+    # ----------------------------------------------------------------------
+
+    # -------------Body Section------------------------------------------------
+    with tab1:
+        default_tab()
+
+                
+    # # -------------Johnson Model Section------------------------------------------------             
+    with tab2:
+         johnson_tab()
+    # # -------------Gatys Model Section------------------------------------------------        
+
