dc6eeb78ddfb9fc089fb841ca26ef97c3953ce46
layo <layo_a@live.com>
2025-09-12 18:20:54 +0100

    feat: added tab fix from stylemotion branch during merge

--- helper/tabs_display.py
+++ helper/tabs_display.py
@@ -4,13 +4,14 @@ from helper.UI_components import example_images, header
 from tabs.johnson_tab import johnson_tab
 from tabs.default_tab import default_tab
 def tabs_display():
-    tab_list : list[str] = ["Main (StyleMotion)"]
-    tab1, = st.tabs(tab_list[0:1])
+    tab_list : list[str] = ["Johnson model"]
+    tab1,  = st.tabs(tab_list)
 
     # -------------Header Section------------------------------------------------
 
     with tab1:
         header()
+
 
 
 
@@ -26,10 +27,9 @@ def tabs_display():
     # ----------------------------------------------------------------------
 
     # -------------Body Section------------------------------------------------
-    with tab1:
-        default_tab()
-
                 
-
+    # # -------------Johnson Model Section------------------------------------------------             
+    with tab1:
+         johnson_tab()
     # # -------------Gatys Model Section------------------------------------------------        
 
