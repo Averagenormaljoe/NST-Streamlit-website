77382af202b4328e1fcd7bd5d3236dcfb3326274
layo <layo_a@live.com>
2025-09-17 10:14:57 +0100

    feat: removed the Gatys tab from the project

--- helper/tabs_display.py
+++ helper/tabs_display.py
@@ -4,8 +4,8 @@ from helper.UI_components import example_images, header
 from tabs.johnson_tab import johnson_tab
 from tabs.default_tab import default_tab
 def tabs_display() -> None:
-    tab_list : list[str] = ["Main (StyleMotion)", "Johnson model", "Gatys model"]
-    tab1, tab2 = st.tabs(tab_list[0:2])
+    tab_list : list[str] = ["Main (StyleMotion)", "Johnson model"]
+    tab1, tab2 = st.tabs(tab_list)
 
     # -------------Header Section------------------------------------------------
 
