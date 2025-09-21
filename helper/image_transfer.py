eab8e8b86b6a2e886bb2e118ca6a99524510bfca
layo <layo_a@live.com>
2025-09-21 21:09:42 +0100

    feat: adding attribution in the project

--- helper/image_transfer.py
+++ helper/image_transfer.py
@@ -1,11 +1,12 @@
 from PIL import Image
 import numpy as np
 import av
-# Code adapted from 'https://github.com/whitphx/style-transfer-web-app/blob/main/input.py'
+
 def frame_to_image(frame: av.VideoFrame, verbose : int = 0) -> np.ndarray:
     if verbose > 0:
         print("frame:", frame, "type:", type(frame))
     return frame.to_ndarray(format="bgr24")
+# Code adapted from 'https://github.com/whitphx/style-transfer-web-app/blob/main/input.py'
 def get_result_image(transferred: np.ndarray, orig_w: int, orig_h: int) -> np.ndarray:
     result : Image = Image.fromarray((transferred * 255).astype(np.uint8))
     image : np.ndarray = np.asarray(result.resize((orig_w, orig_h)))
@@ -13,3 +14,4 @@ def get_result_image(transferred: np.ndarray, orig_w: int, orig_h: int) -> np.nd
 def resize_image(image: np.ndarray, width: int, orig_h: int, orig_w: int) -> np.ndarray:
     input = np.asarray(Image.fromarray(image).resize((width, int(width * orig_h / orig_w))))
     return input
+# end of adapted code
--- helper/turn.py
+++ helper/turn.py
@@ -5,10 +5,15 @@ from typing import Dict, List
 
 import streamlit as st
 from twilio.rest import Client
-
+# Code adapted from https://github.com/whitphx/style-transfer-web-app/blob/main/turn.py
+# Website: GitHub
+# Author: Yuichiro Tachibana (Tsuchiya)
+# Date: Apr 27, 2023
+# Date of access: 21/09/2025
 logger = logging.getLogger(__name__)
 
 
 @st.cache_data 
 def get_ice_servers() -> List[Dict[str, List[str]]]:
     return [{"urls": ["stun:stun.l.google.com:19302"]}]
+# end of code adaption
